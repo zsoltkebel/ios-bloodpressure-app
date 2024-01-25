30a9dd6af130edbe6a83d64e0d4d2f04a4f7d358
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-01-25 08:41:21 +0000

    Initial Commit

---
+++ blood_pressure/blood_pressureApp.swift
@@ -0,0 +1,17 @@
+//
+//  blood_pressureApp.swift
+//  blood_pressure
+//
+//  Created by Zsolt Kébel on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct blood_pressureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
